2c8d8ea6b46131f7fabaf98ca4221d4bc0673069
NamNamju <namuju_hi@naver.com>
2020-01-22 15:43:53 +0900

    fragment

---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RedFragment redFragment = new RedFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_fragment,redFragment).commit();
+    }
+}
